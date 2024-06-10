bcdaf77d00edf487b8a88b1bf709e668d9bb86a1
MINEDUCYT <MINEDUCYT@LAPTOP-7SKAH6B6>
2024-06-10 05:00:58 -0600

    Agregar archivos de proyecto.

---
+++ 2320559vdeo04/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _2320559vdeo04
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 2320559vdeo04/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace _2320559vdeo04
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
